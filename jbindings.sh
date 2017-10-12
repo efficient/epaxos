22b9d9e2347d8395e9988a06aff94d968b96adb5
otrack <0track@gmail.com>
2017-10-12 21:20:08 +0200

    add jbindings build script

--- jbindings.sh
+++ jbindings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+GOPATH=`pwd` go get github.com/sridharv/gojava
 GOPATH=`pwd` gojava -v -o epaxos.jar build bindings
 mvn install:install-file -Dfile=epaxos.jar -DgroupId=epaxos \
     -DartifactId=epaxos -Dversion=1.0 -Dpackaging=jar
