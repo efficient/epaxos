cd265a7c88851dd750a7bfc71181056cb79a1f5f
otrack <0track@gmail.com>
2017-10-14 09:42:08 +0200

    fix jbinding script

--- jbindings.sh
+++ jbindings.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-DIR=$(dirname "$0")
+DIR=`pwd $(dirname "$0")` 
 GOPATH=${DIR} go get github.com/sridharv/gojava
 GOPATH=${DIR} gojava -v -o epaxos.jar build bindings
-mvn -f ${DIR} install:install-file -Dfile=epaxos.jar -DgroupId=epaxos \
-    -DartifactId=epaxos -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=epaxos.jar -DgroupId=epaxos -DartifactId=epaxos -Dversion=1.0 -Dpackaging=jar
