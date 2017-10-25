c98f9ffc77d600b1c950167f85a3a5a744914cd0
otrack <0track@gmail.com>
2017-10-25 07:55:27 +0200

    fix state.go

--- src/state/state.go
+++ src/state/state.go
@@ -2,7 +2,6 @@ package state
 
 import (
 	"sync"
-	"fmt"
 	//"code.google.com/p/leveldb-go/leveldb"
 	//"encoding/binary"
 	"encoding/hex"
@@ -79,7 +78,7 @@ func IsRead(command *Command) bool {
 }
 
 func (c *Command) Execute(st *State) Value {
-	//fmt.Printf("Executing (%d, %d)\n", c.K, c.V)
+	fmt.Printf("Executing (%d, %d)\n", c.K, c.V)
 
 	//var key, value [8]byte
 
