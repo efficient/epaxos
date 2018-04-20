259be61a2ff93d8e11fd4a30c22e93970349a024
Vitor Enes <vitorenesduarte@gmail.com>
2018-04-20 10:36:03 +0100

    Set default docker user when building docker image

--- bin/image.sh
+++ bin/image.sh
@@ -9,6 +9,10 @@ fi
 DOCKER_USER=$(docker info |
               grep Username |
               awk '{print $2}')
+if [ -z "${DOCKER_USER}" ]; then
+  DOCKER_USER=vitorenesduarte
+fi
+
 DIR=$(dirname "$0")
 IMAGE=${DOCKER_USER}/epaxos:${TAG}
 DOCKERFILE=${DIR}/../Dockerfile
