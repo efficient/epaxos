c4be03319c1920a0a40e6ed41647b679a37d6000
Jesse Tuglu <jktuglu@gmail.com>
2021-04-29 22:44:33 -0400

    update

--- src/epaxos/epaxos.go
+++ src/epaxos/epaxos.go
@@ -22,7 +22,7 @@ const FALSE = uint8(0)
 const DS = 5
 const ADAPT_TIME_SEC = 10
 
-const MAX_BATCH = 1000 // switch for testing original=1K
+const MAX_BATCH = 20 // switch for testing original=1K
 
 const COMMIT_GRACE_PERIOD = 10 * 1e9 //10 seconds
 
